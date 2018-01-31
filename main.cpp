c00801dd745b416a6aaa57a2618c3b09fe223b6f
xaegrek <xaegrek@gmail.com>
2018-01-31 19:28:02 +0000

    osdk possible set up

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
+// System Includes
+#include <cmath>
+#include <vector>
+/*
+// DJI OSDK Includes
+#include <dji_vehicle.hpp>
+
+// Helpers
+#include <dji_linux_helpers.hpp>*/
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
