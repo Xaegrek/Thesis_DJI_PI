612165ed90db2395483784887ccab2e87d4d89bb
xaegrek <xaegrek@gmail.com>
2018-02-26 12:17:46 -0500

    removed = sign in waypoint for  loop.  Added output of waypoints for each loop to test. Am experiencing segmantation faults on live system prior to this

--- TestCode/TelemetryControlTest/TelemetryControlTest.cpp
+++ TestCode/TelemetryControlTest/TelemetryControlTest.cpp
@@ -19,10 +19,11 @@ bool trajectoryWaypointControllerTest(DJI::OSDK::Vehicle *vehicle, std::vector<s
 
 
     unsigned long nDim = way.size();
-    for (int nn=0;nn<=nDim;nn=nn+1)
+    for (int nn=0;nn<nDim;nn=nn+1)
     {
         float xOff = way[nn][0]; float yOff = way[nn][1]; float zOff = way[nn][2];
         float yawSpec = way[nn][3];
+        std::cout << "x= "<<xOff<< " ;y= "<<yOff<< " ;z= "<<zOff<<" ;yaw = "<< yawSpec << std::endl;
         moveByPositionOffset(vehicle,xOff,yOff,zOff,yawSpec);
     }
 
