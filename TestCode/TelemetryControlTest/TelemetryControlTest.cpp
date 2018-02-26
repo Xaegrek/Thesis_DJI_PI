162984fa114bfb950957db1ebb0ba1f57805e275
xaegrek <xaegrek@gmail.com>
2018-02-26 13:26:48 -0500

    confused offset with waypoint in code fixed

--- TestCode/TelemetryControlTest/TelemetryControlTest.cpp
+++ TestCode/TelemetryControlTest/TelemetryControlTest.cpp
@@ -23,6 +23,12 @@ bool trajectoryWaypointControllerTest(DJI::OSDK::Vehicle *vehicle, std::vector<s
         float yawSpec = way[nn][3];
         std::cout << "x= "<<xOff<< " ;y= "<<yOff<< " ;z= "<<zOff<<" ;yaw = "<< yawSpec << std::endl;
         moveByPositionOffset(vehicle,xOff,yOff,zOff,yawSpec);
+
+        if (nn!=0)
+        {
+            xOff = xOff - way[nn-1][0]; yOff = yOff - way[nn-1][1]; zOff = zOff - way[nn-1][2];
+        }
+        std::cout << "xc= "<<xOff<< " ;yc= "<<yOff<< " ;zc= "<<zOff<<" ;yaw = "<< yawSpec << std::endl;
     }
 
     monitoredLanding(vehicle);
@@ -39,10 +45,6 @@ bool trajectoryWaypointOffsetControllerTest(DJI::OSDK::Vehicle *vehicle, std::ve
         float xOff = way[nn][0]; float yOff = way[nn][1]; float zOff = way[nn][2];
         float yawSpec = way[nn][3];
         std::cout << "x= "<<xOff<< " ;y= "<<yOff<< " ;z= "<<zOff<<" ;yaw = "<< yawSpec << std::endl;
-        if (nn!=0)
-        {
-            xOff = xOff - way[nn-1][0]; yOff = yOff - way[nn-1][1]; zOff = zOff - way[nn-1][2];
-        }
         moveByPositionOffset(vehicle,xOff,yOff,zOff,yawSpec);
     }
 
